a000c7a1c9830b6369de66204e4d127a90551b25
Rafif Ali Rachman <148606770+Rapipajaaa@users.noreply.github.com>
2023-12-13 09:45:03 +0700

    Upload Rumus

---
+++ Rumus Persegi Panjang/Keliling Persegi Panjang.py
@@ -0,0 +1,6 @@
+def keliling_persegi_panjang(a,b):
+    keliling_persegi_panjang=2*(a+b)
+    return keliling_persegi_panjang
+
+result=keliling_persegi_panjang(10,6)
+print(result)
---
+++ Rumus Persegi Panjang/Luas Persegi Panjang.py
@@ -0,0 +1,6 @@
+def luas_persegi_panjang(a,b):
+    return a*b
+
+#contoh pengerjaan
+result=luas_persegi_panjang(6,10)
+print(result)
